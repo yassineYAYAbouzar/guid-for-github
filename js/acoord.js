149ea03171d6d59f6a46b684635970684f0269e7
yassineYAYAbouzar <barberhb6@gmail.com>
2021-01-06 21:42:51 +0100

    guid for github

---
+++ js/acoord.js
@@ -0,0 +1,26 @@
+let code = document.querySelectorAll('code');
+let frown = document.querySelectorAll('i');
+let start = document.querySelector('.fa-5x');
+let good = document.querySelector('h2');
+code.forEach(co => {
+    co.addEventListener('click',function(e){
+        this.style.background=e.target.dataset.color;
+        this.style.color="#fff";
+        this.lastChild.classList.remove('fa-frown-o');
+        this.lastChild.classList.add('fa-smile-o');
+        this.lastChild.style.color="#f10728"
+        this.lastChild.style.fontSize="21px"
+        this.lastChild.style.fontWeight="bold"
+    
+        if(e.target.dataset.color=="#0a53be"){
+            start.style.textAlign="left";
+            good.style.height="7%";
+        }
+    
+    })
+});
+
+
+
+
+
